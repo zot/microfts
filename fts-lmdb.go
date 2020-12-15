ba48d9a2e54fe73529845dcbee5a3c038f4cb1ec
Bill Burdick <bill.burdick@gmail.com>
2020-12-15 16:22:09 +0200

    making -sexp default to one line per result

--- fts-lmdb.go
+++ fts-lmdb.go
@@ -58,8 +58,8 @@ const (
 	fuzzyLineFormat = "%[6]s:%[1]d:%4.1[4]f%%:%[5]s\n"
 	groupEnd        = ""
 	sexpGroupStart  = ""
-	infoSexpFormat  = "(:filename \"%[6]s\" :line %[2]d :offset %[3]d :text \"%[5]s\")"
-	sexpFormat      = "(:filename \"%[6]s\" :line %[2]d :offset %[3]d :text \"%[5]s\" :percent %[4]f)"
+	infoSexpFormat  = "(:filename \"%[6]s\" :line %[2]d :offset %[3]d :text \"%[5]s\")\n"
+	sexpFormat      = "(:filename \"%[6]s\" :line %[2]d :offset %[3]d :text \"%[5]s\" :percent %[4]f)\n"
 	sexpGroupEnd    = "\n"
 )
 

a445b9b02d85ae2fc6e0638133b90aa23d1bce4e
Bill Burdick <bill.burdick@gmail.com>
2020-12-28 20:25:12 -0500

    fixing names, docs, etc. according to checdock and package-lint

--- elisp/ivy-org-fts.el
+++ elisp/ivy-org-fts.el
@@ -5,83 +5,88 @@
 ;; Author: Bill Burdick <bill.burdick@gmail.com>
 ;; URL: https://github.com/zot/microfts/tree/main/elisp/ivy-org-fts.el
 ;; Version: 1.0.0
-;; Package-Requires: (org-fts ivy)
+;; Package-Requires: ((org-fts "1.0.0") (ivy "0.13.1") (emacs "27.1"))
 ;; Keywords: org convenience ivy
 
 ;;; Commentary:
+
+;; Add ivy support for org-fts
 
 ;;; Code:
 
 (require 'org-fts)
 (require 'ivy)
 
-(defvar ivy-org-fts/hits nil)
-(defvar ivy-org-fts/args nil)
-(defvar ivy-org-fts/history nil)
-(defvar ivy-org-fts/file-history nil)
+(defvar ivy-org-fts-hits nil)
+(defvar ivy-org-fts-args nil)
+(defvar ivy-org-fts-history nil)
+(defvar ivy-org-fts-file-history nil)
 
-(defun ivy-org-fts/found (arg)
+(defun ivy-org-fts-found (arg)
+  "Visit result ARG from an ivy-org-fts search."
   (ignore arg)
-  (let ((hit (elt ivy-org-fts/hits ivy--index)))
+  (let ((hit (elt ivy-org-fts-hits ivy--index)))
     (find-file (plist-get hit :filename))
     (if (equal major-mode 'org-mode) (org-show-all))
     (goto-char (1+ (plist-get hit :char-offset)))))
 
-(defun ivy-org-fts/display (line)
+(defun ivy-org-fts-display (line)
+  "Prepare LINE for display in ivy."
   (let* ((colon1 (cl-search ":" line))
          (colon2 (cl-search ":" line :start2 (1+ colon1)))
          (text (1+ colon2))
          (lowLn (downcase line)))
     (add-text-properties 0 colon1 '(face ivy-grep-info) line)
     (add-text-properties (1+ colon1) colon2 '(face ivy-grep-line-number) line)
-    (cl-do ((a 0 (cl-incf a))) ((>= a (length ivy-org-fts/args)))
-      (let* ((arg (downcase (elt ivy-org-fts/args a))))
+    (cl-do ((a 0 (cl-incf a))) ((>= a (length ivy-org-fts-args)))
+      (let* ((arg (downcase (elt ivy-org-fts-args a))))
         (cl-do ((i (cl-search arg lowLn :start2 text) (cl-search arg lowLn :start2 (+ i (length arg)))))
             ((not i))
           (put-text-property i (+ i (length arg)) 'face 'ivy-minibuffer-match-face-2 line))))
     line))
 
 (ivy-configure 'fts
-  :display-transformer-fn 'ivy-org-fts/display)
+  :display-transformer-fn 'ivy-org-fts-display)
 
-(defun ivy-org-fts-microfs-search (termStr)
-  (let* ((terms (org-fts/split-terms termStr))
-         (lines (org-fts/microfts-basic-search terms)))
-    (setq ivy-org-fts/args terms)
-    (setq ivy-org-fts/hits lines)
+(defun ivy-org-fts-microfs-search (term-str)
+  "Call microfts to search for TERM-STR from ivy."
+  (let* ((terms (org-fts-split-terms term-str))
+         (lines (org-fts-microfts-basic-search terms)))
+    (setq ivy-org-fts-args terms)
+    (setq ivy-org-fts-hits lines)
     (mapcar (lambda (line)
               (let* ((file (plist-get line :filename))
                      (text (replace-regexp-in-string "\n" "\\\\n" (plist-get line :text))))
                 (format "%s:%s: %s" (file-name-base file) (plist-get line :line)  text)))
             lines)))
 
-(defun ivy-org-fts/search ()
-  "Perform an fts search"
+(defun ivy-org-fts-search ()
+  "Perform an fts search."
   (interactive)
-  (when (org-fts/check-db)
-    (call-process org-fts/actual-program nil nil nil
-                  "update" org-fts/db)
+  (when (org-fts-check-db)
+    (call-process org-fts-actual-program nil nil nil
+                  "update" org-fts-db)
     (ivy-read "Org search: " 'ivy-org-fts-microfs-search
-              :history 'ivy-org-fts/history
+              :history 'ivy-org-fts-history
               :dynamic-collection t
-              :action 'ivy-org-fts/found
+              :action 'ivy-org-fts-found
               :caller 'fts)))
 
-(defun ivy-org-fts/find-org-file ()
-  "Find one of your org files"
+(defun ivy-org-fts-find-org-file ()
+  "Find one of your org files."
   (interactive)
-  (when (org-fts/check-db)
+  (when (org-fts-check-db)
     (with-current-buffer (get-buffer-create "org-fts")
       (erase-buffer)
-      (call-process org-fts/actual-program nil "org-fts" nil
-                    "info" "-groups" org-fts/db)
+      (call-process org-fts-actual-program nil "org-fts" nil
+                    "info" "-groups" org-fts-db)
       (goto-char (point-min))
       (perform-replace " *\\(org-mode\\)?\\( DELETED\\| NOT AVAILABLE\\| CHANGED\\)?$" "" nil t nil)
       (let ((files (seq-filter (lambda (x) (> (length x) 0)) (split-string (buffer-string) "\n"))))
         (ivy-read "Find org file: " (seq-sort (lambda (a b)
                                                 (string-collate-lessp
                                                  (downcase a) (downcase b))) files)
-                  :history ivy-org-fts/file-history
+                  :history ivy-org-fts-file-history
                   :action 'find-file)))))
 
 (provide 'ivy-org-fts)
--- elisp/org-fts.el
+++ elisp/org-fts.el
@@ -5,10 +5,20 @@
 ;; Author: Bill Burdick <bill.burdick@gmail.com>
 ;; URL: https://github.com/zot/microfts/tree/main/elisp/org-fts.el
 ;; Version: 1.0.0
-;; Package-Requires: (cl-lib org package executable)
+;; Package-Requires: ((org "9.1.0") (emacs "25.1"))
 ;; Keywords: org convenience
 
 ;;; Commentary:
+
+;; Org-fts automatically indexes changed org files as you visit them
+;; and save them. If you delete or rename an org file, org-fts will
+;; lose track of it until you visit it again.
+
+;; Searching is based on microfts, which is fairly fast, (returning
+;; results in less than 10ms for around 3M of files). Full text
+;; indexing takes space, so expect the index to take maybe 2-5 times
+;; the space of your files. Each keystroke in the screencast below
+;; does another full text search with microfts.
 
 ;;; Code:
 
@@ -21,134 +31,137 @@
   "Customization for org-fts"
   :group 'org)
 
-(defconst org-fts/microfts-url-alist 
+(defconst org-fts-microfts-url-alist
   '((gnu/linux . "https://github.com/zot/microfts/releases/download/v1.0.0/microfts-linux.gz")
     (windows-nt . "https://github.com/zot/microfts/releases/download/v1.0.0/microfts-windows.gz")
     (darwin . "https://github.com/zot/microfts/releases/download/v1.0.0/microfts-mac.gz")))
 
-(defconst org-fts/baseprogram-alist
+(defconst org-fts-baseprogram-alist
   '((gnu/linux . "microfts")
     (darwin . "microfts")
     (windows-nt . "microfts.exe")))
 
-(defconst org-fts/baseprogram
-  (let ((name (cdr (assoc system-type org-fts/baseprogram-alist))))
+(defconst org-fts-baseprogram
+  (let ((name (cdr (assoc system-type org-fts-baseprogram-alist))))
     (format "%s%s" user-emacs-directory name)))
 
-(defcustom org-fts/program nil
-  "Name or path for microfts program"
+(defcustom org-fts-program nil
+  "Name or path for microfts program."
   :type 'string
   :group 'org-fts)
 
-(defcustom org-fts/db (file-truename (format "%sorg-fts.db" user-emacs-directory))
-  "Path to the org-fts database file"
+(defcustom org-fts-db (file-truename (format "%sorg-fts.db" user-emacs-directory))
+  "Path to the org-fts database file."
   :type 'string
   :group 'org-fts)
 
-(defcustom org-fts/search-args '("-partial")
-  "Default arguments to search"
+(defcustom org-fts-search-args '("-partial")
+  "Default arguments to search."
   :type '(list string)
   :group 'org-fts)
 
-(defvar org-fts/timer (run-with-idle-timer (* 60 5) t 'org-fts/idle-task))
-(defvar org-fts/actual-program nil)
+(defvar org-fts-timer (run-with-idle-timer (* 60 5) t 'org-fts-idle-task))
+(defvar org-fts-actual-program nil)
 
-(defun org-fts/check-db ()
-  "create fts db if it is not there"
-  (and (org-fts/test org-fts/db "need to customize org-fts/db, it has no value")
-       (org-fts/ensure-binary)
-       (or (file-exists-p org-fts/db)
-           (let ((ret (call-process org-fts/actual-program nil nil nil
-                                    "create" org-fts/db)))
-             (org-fts/test (eql 0 ret) "creating database %s returned error %s" org-fts/db ret)))))
+(defun org-fts-check-db ()
+  "Create fts db if it is not there."
+  (and (org-fts-test org-fts-db "need to customize org-fts-db, it has no value")
+       (org-fts-ensure-binary)
+       (or (file-exists-p org-fts-db)
+           (let ((ret (call-process org-fts-actual-program nil nil nil
+                                    "create" org-fts-db)))
+             (org-fts-test (eql 0 ret) "creating database %s returned error %s" org-fts-db ret)))))
 
-(defun org-fts/test (item &rest format)
-  "print a message if a test fails"
-  (or item
+(defun org-fts-test (test &rest format)
+  "If TEST fails, use message to print FORMAT args."
+  (or test
       (progn
         (apply 'message format)
         nil)))
 
-(defun org-fts/save-hook ()
-  "A file was just saved, index/reindex it if it's an org-mode file"
+(defun org-fts-save-hook ()
+  "A file was just saved, index/reindex it if it's an `org-mode' file."
   (when (and
          (buffer-file-name)
 	     (equal major-mode 'org-mode)
-         (org-fts/check-db))
+         (org-fts-check-db))
     (message "UPDATING ORG-FTS: %s" (file-truename (buffer-file-name)))
-    (start-process "org-fts" nil org-fts/actual-program
-                   "input" "-org" org-fts/db (file-truename (buffer-file-name)))))
+    (start-process "org-fts" nil org-fts-actual-program
+                   "input" "-org" org-fts-db (file-truename (buffer-file-name)))))
 
-(defun org-fts/open-hook ()
-  "A file was just saved, index/reindex it if it's an org-mode file"
+(defun org-fts-open-hook ()
+  "A file was just saved, index/reindex it if it's an `org-mode' file."
   (when (and
          (buffer-file-name)
 	     (equal major-mode 'org-mode)
-         (org-fts/check-db))
+         (org-fts-check-db))
     (message "UPDATING ORG-FTS: %s" (file-truename (buffer-file-name)))
     (let* ((file (file-truename (buffer-file-name)))
-          (ret (call-process org-fts/actual-program nil nil nil
-                             "empty" "-org" org-fts/db file)))
-      (org-fts/test (eql 0 ret) "adding %s to returned error %s" file ret)
-      (start-process "org-fts" nil org-fts/actual-program
-                     "update" org-fts/db))))
+          (ret (call-process org-fts-actual-program nil nil nil
+                             "empty" "-org" org-fts-db file)))
+      (org-fts-test (eql 0 ret) "adding %s to returned error %s" file ret)
+      (start-process "org-fts" nil org-fts-actual-program
+                     "update" org-fts-db))))
 
-(add-hook 'org-mode-hook 'org-fts/open-hook)
-(add-hook 'after-save-hook 'org-fts/save-hook)
+(add-hook 'org-mode-hook 'org-fts-open-hook)
+(add-hook 'after-save-hook 'org-fts-save-hook)
 
-(defun org-fts/idle-task ()
-  "After a certain amount of idle time, update the org-fts database"
-  (when (org-fts/check-db)
-    (let ((process (start-process "org-fts" nil org-fts/actual-program
-                                  "update" org-fts/db)))
+(defun org-fts-idle-task ()
+  "After a certain amount of idle time, update the org-fts database."
+  (when (org-fts-check-db)
+    (let ((process (start-process "org-fts" nil org-fts-actual-program
+                                  "update" org-fts-db)))
       (set-process-sentinel process
                             (lambda (process status)
                               (ignore process)
                               (when (equal status "finished\n")
-                                (start-process "org-fts" nil org-fts/actual-program
-                                               "compact" org-fts/db)))))))
+                                (start-process "org-fts" nil org-fts-actual-program
+                                               "compact" org-fts-db)))))))
 
-(defun org-fts/microfts-basic-search (terms)
-  (when (and (org-fts/test terms "EMPTY TERMS") (org-fts/check-db))
+(defun org-fts-microfts-basic-search (terms)
+  "Call microfts with TERMS and return results."
+  (when (and (org-fts-test terms "EMPTY TERMS") (org-fts-check-db))
     (with-current-buffer (get-buffer-create "org-search")
       (erase-buffer)
-      (let ((ret (apply #'call-process org-fts/actual-program nil "org-search" nil
-                        `("search" "-sexp" ,@org-fts/search-args ,org-fts/db ,@terms))))
+      (let ((ret (apply #'call-process org-fts-actual-program nil "org-search" nil
+                        `("search" "-sexp" ,@org-fts-search-args ,org-fts-db ,@terms))))
         (if (not (eql ret 0))
             (progn
               (message "Search returned error %s" ret)
               nil)
           (mapcar 'read (split-string (buffer-string) "\n" t)))))))
 
-(defun org-fts/split-terms (termStr)
-  (condition-case nil (split-string-and-unquote termStr) ((debug error) nil)))
+(defun org-fts-split-terms (term-str)
+  "Split TERM-STR into parts, respecting quotes."
+  (condition-case nil (split-string-and-unquote term-str) ((debug error) nil)))
 
-(defun org-fts/microfts-search (termStr)
+(defun org-fts-microfts-search (term-str)
+  "Non-fancy search for TERM-STR."
   (mapcar (lambda (line)
             (let ((file (plist-get line :filename))
                   (text (replace-regexp-in-string "\n" "\\\\n" (plist-get line :text))))
               (format "%s:%s: %s" file (plist-get line :line)  text)))
-          (org-fts/microfts-basic-search (org-fts/split-terms termStr))))
+          (org-fts-microfts-basic-search (org-fts-split-terms term-str))))
 
-(defun org-fts/ensure-binary ()
-  "Make sure microfts is present"
-  (setq org-fts/actual-program
+(defun org-fts-ensure-binary ()
+  "Make sure microfts is present."
+  (setq org-fts-actual-program
         (or
-         (and org-fts/program
-              (or (file-exists-p org-fts/program)
-                  (error "Program not found: %s" org-fts/program))
-              (or (file-executable-p org-fts/program)
-                  (error "Program exists but is not executable: %s" org-fts/program))
-              org-fts/program)
-         (and (file-exists-p org-fts/baseprogram)
-              (or (file-executable-p org-fts/baseprogram)
-                  (error "Program exists but is not executable: %s" org-fts/baseprogram))
-              org-fts/baseprogram)
+         (and org-fts-program
+              (or (file-exists-p org-fts-program)
+                  (error "Program not found: %s" org-fts-program))
+              (or (file-executable-p org-fts-program)
+                  (error "Program exists but is not executable: %s" org-fts-program))
+              org-fts-program)
+         (and (file-exists-p org-fts-baseprogram)
+              (or (file-executable-p org-fts-baseprogram)
+                  (error "Program exists but is not executable: %s" org-fts-baseprogram))
+              org-fts-baseprogram)
          ;; if baseprogram is not there, download it
-         (let* ((url (cdr (assoc system-type org-fts/microfts-url-alist)))
-                (comprfile (format "%s.gz" org-fts/baseprogram))
+         (let* ((url (cdr (assoc system-type org-fts-microfts-url-alist)))
+                (comprfile (format "%s.gz" org-fts-baseprogram))
                 (compr auto-compression-mode))
-           (when (not org-fts/baseprogram) (error "No value for org-fts/baseprogram or org-fts/program"))
+           (when (not org-fts-baseprogram) (error "No value for org-fts-baseprogram or org-fts-program"))
            (when (not url) (error "Unsupported system type: %s" system-type))
            (url-copy-file url comprfile t)
            (save-excursion
@@ -159,25 +172,25 @@
                    (zlib-decompress-region (point-min) (point-max))
                    (write-file comprfile)
                    (executable-chmod)
-                   (rename-file comprfile org-fts/baseprogram)
+                   (rename-file comprfile org-fts-baseprogram)
                    (kill-buffer)
-                   org-fts/baseprogram)
+                   org-fts-baseprogram)
                (if compr (auto-compression-mode 1))))))))
 
-(defun org-fts/search (terms)
-  "Search org files for terms"
+(defun org-fts-search (terms)
+  "Search org files for TERMS."
   (interactive "sOrg search: ")
   (minibuffer-with-setup-hook (lambda ())
-    (completing-read "Org file results: " (org-fts/microfts-search terms))))
+    (completing-read "Org file results: " (org-fts-microfts-search terms))))
 
-(defun org-fts/find-org-file ()
-  "Find one of your org files"
+(defun org-fts-find-org-file ()
+  "Find one of your org files."
   (interactive)
-  (when (org-fts/check-db)
+  (when (org-fts-check-db)
     (with-current-buffer (get-buffer-create "org-fts")
       (erase-buffer)
-      (call-process org-fts/actual-program nil "org-fts" nil
-                    "info" "-groups" org-fts/db)
+      (call-process org-fts-actual-program nil "org-fts" nil
+                    "info" "-groups" org-fts-db)
       (goto-char (point-min))
       (perform-replace " *\\(org-mode\\)?\\( DELETED\\| NOT AVAILABLE\\| CHANGED\\)?$" "" nil t nil)
       (let ((files (seq-filter (lambda (x) (> (length x) 0)) (split-string (buffer-string) "\n"))))
